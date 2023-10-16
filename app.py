ee5ed8aec41b679f86188f0ebfc2abdd77bcd5ef
EC2 Default User <ec2-user@ip-172-31-18-53.ec2.internal>
2023-10-16 03:51:43 +0000

    funcion final del taller

--- app.py
+++ app.py
@@ -247,5 +247,5 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', port=8050)
+    app.run_server(host='0.0.0.0', debug=True)
     
